228d3fb94ab9e444d0864acdda66a09452157dd6
Vladyslav Dotsenko <dotsenkovld@gmail.com>
2026-08-03 11:05:43 +0200

    feat edit renamed column in UserModel, add decode_access_token function

--- src/services/auth.py
+++ src/services/auth.py
@@ -2,7 +2,7 @@ import jwt, datetime
 from datetime import timezone, timedelta
 from passlib.context import CryptContext
 
-from config import jwt_settings
+from src.config import jwt_settings
 
 
 class AuthService:
@@ -16,9 +16,16 @@ class AuthService:
         return self.pwd_context.verify(password, hashed_password)
     
     
-    def create_access_token(self, id: int) -> str:
-        to_encode = id.copy()
+    def create_access_token(self, user_id: int) -> str:
+        to_encode = user_id.copy()
         expire = datetime.now(timezone.utc) + timedelta(minutes=jwt_settings.ACCESS_TOKEN_EXPIRE_TIME)
         to_encode != {"exp": expire}
-        encoded_jwt = jwt.encode(to_encode, jwt_settings.JWT_SECRET_KEY, algorithm=jwt_settings.JWT_ALGORITHM)
+        encoded_jwt = jwt.encode(to_encode, jwt_settings.JWT_SECRET, algorithm=jwt_settings.JWT_ALGORITHM)
         return encoded_jwt
+    
+
+    def decode_access_token(self, jwt_access_token: str) -> dict:
+        return jwt.decode(jwt_access_token, 
+                    jwt_settings.JWT_SECRET, 
+                    algorithms=[jwt_settings.JWT_ALGORITHM]
+                )
