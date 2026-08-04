ba253ba5884be2a466a6e1635cb2976c38972288
Vladyslav Dotsenko <dotsenkovld@gmail.com>
2026-08-04 22:58:39 +0200

    feat added create_book endpoint

--- src/api/book.py
+++ src/api/book.py
@@ -7,6 +7,19 @@ from src.dependencies.database import DBDep
 router = APIRouter(prefix="/book", tags=["book"])
 
 
+@router.post("", 
+          summary="Create book", 
+          description="Adds a new book to the collection"
+          )
+async def create_book(
+    db: DBDep,
+    book: BookAdd
+):
+  await db.book.add(book)
+  await db.commit()
+  return {"status": "OK", "data": book}
+
+
 @router.get("/{book_id}",
             summary="Get book by id",
             description="Retrieves a book by its ID"
@@ -15,13 +28,4 @@ async def get_book_by_id(
     db: DBDep,
     book_id: int
 ):
-    return await db.get_one_or_none(id=book_id)
-
-
-@router.post("", 
-          summary="Create book", 
-          description="Adds a new book to the collection"
-          )
-async def create_book():
-    ...
-    
+    return await db.get_one_or_none(id=book_id)    
