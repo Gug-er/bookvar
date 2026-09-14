cd74d46bc609b0cedd9ea9fbe3bb4ba5a527eaf3
Vladyslav Dotsenko <dotsenkovld@gmail.com>
2026-09-14 20:10:13 +0200

    feat created register and login api tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 import pytest
+from httpx import AsyncClient, ASGITransport
+
+from main import app
 
 from src.config import db_settings
 from src.db_engine import Base, engine_null_pool
@@ -11,7 +14,21 @@ async def check_conn_mode():
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main(check_conn_mode):
+async def setup_database(check_conn_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+        
+        
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(setup_database):
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        await ac.post(
+            "/auth/register",
+            json = {
+                "email" : "test@user.com",
+                "password" : "testuserpassword",
+                "first_name" : "Test",
+                "last_name" : "User"
+            }
+        )
